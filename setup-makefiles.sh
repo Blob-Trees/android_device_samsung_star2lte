d9d085c5d083afa5d158e1d5151af037ca82703b
Dylan Neve <dylanneve1@gmail.com>
2021-12-31 00:17:33 +0000

    star2lte: Update extract-files and setup-makefiles

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -17,11 +17,32 @@
 
 set -e
 
-# Required!
-export DEVICE=star2lte
-export DEVICE_COMMON=universal9810-common
-export VENDOR=samsung
+VENDOR=samsung
+DEVICE=star2lte
 
-export DEVICE_BRINGUP_YEAR=2018
+export INITIAL_COPYRIGHT_YEAR=2020
 
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+LINEAGE_ROOT="${MY_DIR}/../../.."
+
+HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
+
+# Copyright headers and guards
+write_headers "star2lte"
+
+# The standard blobs
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Finish
+write_footers
